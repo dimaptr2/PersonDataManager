9a5d31d725d5d44865152410c0d821d87b34bc30
dimaptr2 <dimaptr2@gmail.com>
2018-09-14 16:43:22 +0300

    14/09/2018 P Watcher (simple spring application)

--- src/main/java/ru/iegs/services/person/watcher/controller/rest/ServiceProcessor.java
+++ src/main/java/ru/iegs/services/person/watcher/controller/rest/ServiceProcessor.java
@@ -1,17 +1,37 @@
 package ru.iegs.services.person.watcher.controller.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ru.iegs.services.person.watcher.controller.managers.PersonDataManager;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class ServiceProcessor {
 
+    private static final String TEMPLATE_TEXT = "%s";
+    private final AtomicLong counter = new AtomicLong();
     private PersonDataManager personDataManager;
 
     @Autowired
     public ServiceProcessor(PersonDataManager personDataManager) {
         this.personDataManager = personDataManager;
+    }
+
+    @PostMapping("/person/store")
+    public String storePersonEntity(
+            @RequestParam(name = "first-name", defaultValue = "") String firstName,
+            @RequestParam(name = "last-name", defaultValue = "") String lastName,
+            @RequestParam(name = "middle-name", defaultValue = "") String middleName) {
+
+        StringBuilder answer = new StringBuilder(0);
+        if (firstName.isEmpty()) {
+
+        }
+
+        return answer.toString();
     }
 
 }
