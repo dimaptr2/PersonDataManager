0bdb49419c5553d1f8e4837d3ef7ba3ab02d13bb
dimaptr2 <dimaptr2@gmail.com>
2018-09-14 16:53:02 +0300

    14/09/2018 P Watcher (simple spring application)

--- src/main/java/ru/iegs/services/person/watcher/controller/rest/ServiceProcessor.java
+++ src/main/java/ru/iegs/services/person/watcher/controller/rest/ServiceProcessor.java
@@ -2,6 +2,7 @@ package ru.iegs.services.person.watcher.controller.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ru.iegs.services.person.watcher.controller.managers.PersonDataManager;
